d85164b65d394242389770efcdcaec33ded1b827
Varun <varunshaji44@gmail.com>
2025-03-05 12:12:06 +0530

    Basic Chores

--- src/main/java/com/taskmanagement/task/Repository/NoteRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/NoteRepository.java
@@ -1,8 +1,9 @@
 package com.taskmanagement.task.Repository;
+
+import com.taskmanagement.task.Entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    
 }
--- src/main/java/com/taskmanagement/task/Service/NoteService.java
+++ src/main/java/com/taskmanagement/task/Service/NoteService.java
@@ -1,8 +1,15 @@
 package com.taskmanagement.task.Service;
+import com.taskmanagement.task.Repository.NoteRepository;
+import com.taskmanagement.task.DTO.NoteDTO;
+import com.taskmanagement.task.Entity.Note;
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
+
+
 
 @Service
 public class NoteService {
