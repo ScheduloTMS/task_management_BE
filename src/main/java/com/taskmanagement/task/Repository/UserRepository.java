fc25807f7f092ac36af34bbfa52c536a50287efa
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-11 15:38:20 +0530

    feat: add query to fetch user details with photo

--- src/main/java/com/taskmanagement/task/Repository/UserRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/UserRepository.java
@@ -3,15 +3,23 @@ package com.taskmanagement.task.Repository;
 import com.taskmanagement.task.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, String> {
-    Optional<Users> findByUserId(String userId);
 
-    @Query("SELECT MAX(u.userId) FROM Users u WHERE u.userId LIKE ?1%")
-    String findLastUserIdByRole(String rolePrefix);
+    @Query("SELECT u FROM Users u WHERE u.userId = :userId")
+    Optional<Users> findByUserId(@Param("userId") String userId);
 
+
+    @Query("SELECT u FROM Users u LEFT JOIN FETCH u.photo WHERE u.userId = :userId")
+    Optional<Users> findByIdWithPhoto(@Param("userId") String userId);
+
+
+    @Query("SELECT u.userId FROM Users u WHERE u.userId LIKE :rolePrefix% ORDER BY u.userId DESC LIMIT 1")
+    String findLastUserIdByRole(@Param("rolePrefix") String rolePrefix);
 
 
     List<Users> findAllByDeletedAtIsNull();
