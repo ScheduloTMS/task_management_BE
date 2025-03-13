7141579c596be21896212531c43741b0da822f69
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-13 17:57:29 +0530

    feat: update in code

--- src/main/java/com/taskmanagement/task/Security/CustomUserDetailsService.java
+++ src/main/java/com/taskmanagement/task/Security/CustomUserDetailsService.java
@@ -3,11 +3,14 @@ package com.taskmanagement.task.Security;
 import com.taskmanagement.task.Entity.Users;
 import com.taskmanagement.task.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,10 +23,11 @@ public class CustomUserDetailsService implements UserDetailsService {
         Users users = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("Users not found with userId: " + userId));
 
-        return User.builder()
-                .username(users.getUserId())
-                .password(users.getPassword())
-                .roles("ROLE_" + users.getRole())
-                .build();
+        return new User(
+                users.getUserId(),
+                users.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + users.getRole()))
+        );
+
     }
 }
