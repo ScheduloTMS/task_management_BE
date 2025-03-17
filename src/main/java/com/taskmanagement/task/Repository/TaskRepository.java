5e2ae86196a468797a3e85ea479234e4070776be
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-17 12:52:04 +0530

    feat: add taskrepo

--- src/main/java/com/taskmanagement/task/Repository/TaskRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/TaskRepository.java
@@ -1,4 +1,19 @@
 package com.taskmanagement.task.Repository;
 
-public class TaskRepository {
+import com.taskmanagement.task.Entity.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
+
+    List<TaskEntity> findByDeletedAtIsNull();
+    List<TaskEntity> findByTitleIgnoreCaseAndDescriptionIgnoreCaseAndDueDate(String title, String description, LocalDate dueDate);
+
+    Optional<TaskEntity> findByTaskIdAndDeletedAtIsNull(UUID taskId);
 }
