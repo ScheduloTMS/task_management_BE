bf4046ba483397ab7171dc0ce913785fba5c79b8
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-18 12:38:32 +0530

    feat: add assigndto

--- src/main/java/com/taskmanagement/task/DTO/StudentAssignmentRequest.java
+++ src/main/java/com/taskmanagement/task/DTO/StudentAssignmentRequest.java
@@ -1,4 +1,16 @@
 package com.taskmanagement.task.DTO;
 
+import java.util.List;
+
 public class StudentAssignmentRequest {
+    private List<String> studentIds;
+
+
+    public List<String> getStudentIds() {
+        return studentIds;
+    }
+
+    public void setStudentIds(List<String> studentIds) {
+        this.studentIds = studentIds;
+    }
 }
