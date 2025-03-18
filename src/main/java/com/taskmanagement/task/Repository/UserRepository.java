f58b8ee58a475de8aa2284da4e22024ff82513b7
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-18 15:13:10 +0530

    feat: add user entity operations

--- src/main/java/com/taskmanagement/task/Repository/UserRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -13,11 +12,12 @@ public interface UserRepository extends JpaRepository<Users, String> {
 
     Optional<Users> findByUserId(String userId);
 
-
     @Query("SELECT u.userId FROM Users u WHERE u.userId LIKE :rolePrefix% ORDER BY u.userId DESC LIMIT 1")
     String findLastUserIdByRole(@Param("rolePrefix") String rolePrefix);
 
     @Query("SELECT u FROM Users u WHERE u.deletedAt IS NULL")
     List<Users> findAllActiveUsers();
 
+
+    Optional<Users> findByUserIdAndDeletedAtIsNull(String userId);
 }
