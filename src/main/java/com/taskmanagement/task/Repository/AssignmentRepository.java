bc547306fb8836cb92ed93ed89076ad01ae4ac23
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-18 12:39:37 +0530

    feat: add assignrepo

--- src/main/java/com/taskmanagement/task/Repository/AssignmentRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/AssignmentRepository.java
@@ -1,4 +1,12 @@
 package com.taskmanagement.task.Repository;
 
-public class AssignmentRepository {
+import com.taskmanagement.task.Entity.AssignmentEntity;
+import com.taskmanagement.task.Entity.AssignmentId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface AssignmentRepository extends JpaRepository<AssignmentEntity, AssignmentId> {
+    Optional<AssignmentEntity> findById(AssignmentId id);
 }
