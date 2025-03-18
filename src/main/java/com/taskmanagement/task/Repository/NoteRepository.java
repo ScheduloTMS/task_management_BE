826e64de5e92755feea5dee994ac49163d7e11d0
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-18 15:08:36 +0530

    feat: add noterepo

--- src/main/java/com/taskmanagement/task/Repository/NoteRepository.java
+++ src/main/java/com/taskmanagement/task/Repository/NoteRepository.java
@@ -1,4 +1,13 @@
 package com.taskmanagement.task.Repository;
 
-public class NoteRepository {
+import com.taskmanagement.task.Entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, UUID> {
+    List<Note> findByUser_UserIdAndDeletedAtIsNull(String userId);
 }
