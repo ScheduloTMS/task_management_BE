a446a799ed7e8dba57d818d3c87ee5346fb5702a
jyothirmayiks <jothirmayiks@gmail.com>
2025-03-18 12:39:08 +0530

    feat: add mapping entity

--- src/main/java/com/taskmanagement/task/Entity/AssignmentId.java
+++ src/main/java/com/taskmanagement/task/Entity/AssignmentId.java
@@ -1,4 +1,25 @@
 package com.taskmanagement.task.Entity;
 
-public class AssignmentId {
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+public class AssignmentId implements Serializable {
+    private UUID taskId;
+    private String userId;
+
+    public AssignmentId() {}
+
+    public AssignmentId(UUID taskId, String userId) {
+        this.taskId = taskId;
+        this.userId = userId;
+    }
+
+
+    public UUID getTaskId() { return taskId; }
+    public void setTaskId(UUID taskId) { this.taskId = taskId; }
+
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
 }
